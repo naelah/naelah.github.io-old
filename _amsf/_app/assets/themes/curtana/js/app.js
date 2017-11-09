133ecb576b3a7a31be48dfad897e37d62fc0f1de
naelah <naelahnordin@gmail.com>
2017-11-09 15:24:07 +0900

    change baseline

--- _amsf/_app/assets/themes/curtana/js/app.js
+++ _amsf/_app/assets/themes/curtana/js/app.js
@@ -1,1 +1,4 @@
-navigator.platform.match(/(Mac|iPhone|iPod|iPad)/i)&&document.body.classList.add("js-system--apple");
+// Detect user platform
+if (navigator.platform.match(/(Mac|iPhone|iPod|iPad)/i)) {
+  document.body.classList.add('js-system--apple');
+}
